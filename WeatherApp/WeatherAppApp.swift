11ddb6c2b7c944f1ef47c287a47a543e4ff5a72b
Yeldos99 <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>
2026-02-11 11:17:38 +0500

    Initial commit: empty SwiftUI project

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Eldos Kolbay on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
